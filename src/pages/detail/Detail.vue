1f071d47c7d19e6b4f21f3bbc23198f1a162a866
yangmiemie <1094759234@qq.com>
2019-04-01 14:26:53 +0800

    'detail-list'

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
     <div>
         <detail-banner></detail-banner>
         <detailh-header></detailh-header>
+        <detail-list :list="list"></detail-list>
         <div class="content"></div>
     </div>
 </template>
@@ -9,11 +10,44 @@
 <script>
 import DetailBanner from './components/banner.vue'
 import DetailhHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
 export default {
   name: 'detail',
   components: {
     DetailBanner: DetailBanner,
-    DetailhHeader: DetailhHeader
+    DetailhHeader: DetailhHeader,
+    DetailList: DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三馆联票',
+              children: [
+                {
+                  title: '成人三馆联票连锁店销售'
+                }
+              ]
+            },
+            {
+              title: '成人五馆联票'
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
